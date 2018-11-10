020c95fd98ee04cf444bec56d6aeb33eba8e4de9
QuentinEPFL <44364155+QuentinEPFL@users.noreply.github.com>
2018-11-10 14:36:42 +0100

    Adding doxygen comments

--- BedReader/src/BEDreader.h
+++ BedReader/src/BEDreader.h
@@ -2,9 +2,11 @@
 #define BEDREADER_H
 #include <string> 
 #include <vector>
-using namespace std;
 
-
+/*!
+  This is a class allowing to read a BED file and to access to each one of the elements it contains.
+ */
+ 
 class bedreader
 {
 	private :
@@ -17,12 +19,21 @@ class bedreader
 	
 	
 	public :
+	
+	/*! Constructor 
+		@param[in] the name of the file given by the user */
 	bedreader(string filename);
+	
+	/*! Reads the file and stocks the elements corresponding to the sequence name given by the user in a vector<vector<string>> */
 	void Read();
+	
+	/*! Sets the name of the sequence the user is looking for.
+		@param[in] the name of the sequence the user is looking for */
 	void setName(string nomEntre);
+	
+	/*! Get lignes.
+		@param[out] lignes : vector<vector<string>> cointaining each line corresponding to the sequence we want parsed into a vector<string> */
 	vector<vector<string>> getlignes();
-	
-	
 	
 };
 #endif
